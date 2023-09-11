950c7e9856e7d30ed293a4285d288fd2287bc4d1
ovgofman <o.v.gofman@gmail.com>
2023-09-11 13:06:18 +0300

    Include into the module 'first_word', and 'reverse' functions.

--- lesson3_task2.erl
+++ lesson3_task2.erl
@@ -4,7 +4,7 @@
 %%% @doc
 %%% Maps String to List of Words.
 %%% @end
-%%% Created : 05. Aug 2023 1:16 PM
+%%% Created : 05. Sep 2023 1:16 PM
 %%%-------------------------------------------------------------------
 -module(lesson3_task2).
 -author("alex").
@@ -13,8 +13,6 @@
 -export([               % Input -> Output
   words/1               % <<"Mama Dada Hi Hello Bye More No">> -> <<"Mama","Dada","Hi","Hello","Bye","More","No">>
 ]).
--import(lesson3_task1, [first_word/1]).
--import(lesson2_task05, [reverse/1]).
 
 %% @spec 1. String to list of words
 words(Bin) ->
@@ -25,9 +23,9 @@ to_list_of_bins(String, List_of_strings) ->
   Word = first_word(Trimmed_String),
   L = len(first_word(Word)),
   case Trimmed_String of
-    <<>> ->
+    <<>> -> 
       List_of_strings;
-    <<X:L/binary, Rest/binary>> when X =:= Word ->
+    <<X:L/binary, Rest/binary>> when X =:= Word -> 
       to_list_of_bins(Rest, [X | List_of_strings])
   end.
 
@@ -39,8 +37,28 @@ trim(Bin) ->
 
 len(L) ->
   cnt(L, 0).
+
 cnt(L, N) ->
   case L of
     <<_, T/binary>> -> cnt(T, N + 1);
     _ -> N
+  end.
+
+first_word(Bin) ->
+  first_word(Bin, <<>>).
+first_word(String, Word) ->
+  case String of
+    <<>> -> Word;
+    <<$\s, T/binary>> -> first_word(T, Word);
+    <<H/utf8, $\s, _/binary>> -> first_word(<<>>, <<Word/binary, H/utf8>>);
+    <<H/utf8, T/binary>> -> first_word(T, <<Word/binary, H/utf8>>)
+  end.
+
+reverse(L) ->
+  reverse(L, []).
+reverse(L, R) ->
+  case L of
+    [H | []] -> [H | R];
+    [H | T] -> reverse(T, [H | R]);
+    [] -> R
   end.
